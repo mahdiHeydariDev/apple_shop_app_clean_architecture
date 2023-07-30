8fd2a8305449dd97aa4fc61e0dd3172bbe4c3daa
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-07-30 14:05:23 +0330

    create products use case

---
+++ lib/features/store_feature/domain/use_cases/products_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_app_clean_architecture/core/utils/errors/custom_error.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/products_repository_impl.dart';
+
+class ProductsUsecase {
+  final ProductsRepositoryImpl repository;
+  ProductsUsecase({required this.repository});
+  Future<Either<CustomError, List<ProductEntity>>> call(
+      {required String filter}) async {
+    return await repository.getFilteredProducts(filter: filter);
+  }
+}
