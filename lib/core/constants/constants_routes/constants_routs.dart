02f0859cfd0b50a03fb4f333934140f51da7b1a4
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-07-30 14:06:00 +0330

    add products routes

--- lib/core/constants/constants_routes/constants_routs.dart
+++ lib/core/constants/constants_routes/constants_routs.dart
@@ -1,4 +1,8 @@
 class ConstantsRoutesApi {
+  //get all banners
   static const String bannersRoutes = 'collections/banner/records';
+  //get all categories
   static const String categoryRoutes = 'collections/category/records';
+  //get all products
+  static const String productsRoutes = 'collections/products/records';
 }
