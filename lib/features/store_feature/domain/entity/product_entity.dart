00e70d416104be80ed6fce666f56207064ce00b3
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-07-30 14:03:51 +0330

    create product exntity class

---
+++ lib/features/store_feature/domain/entity/product_entity.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class ProductEntity extends Equatable {
+  final String category;
+  final String description;
+  final int discountPrice;
+  final String id;
+  final String name;
+  final String popularity;
+  final int price;
+  final num realPrice;
+  final num percent;
+  final int quantity;
+  final String thumbnail;
+  const ProductEntity({
+    required this.category,
+    required this.description,
+    required this.discountPrice,
+    required this.id,
+    required this.name,
+    required this.popularity,
+    required this.price,
+    required this.realPrice,
+    required this.percent,
+    required this.quantity,
+    required this.thumbnail,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        category,
+        description,
+        discountPrice,
+        id,
+        name,
+        popularity,
+        price,
+        quantity,
+        thumbnail,
+      ];
+}
