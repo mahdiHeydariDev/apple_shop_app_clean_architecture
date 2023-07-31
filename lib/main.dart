65c1e22639734a13daa4dd131c1cbd976800aeba
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-07-31 10:56:14 +0330

    add bottom navigation widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app_clean_architecture/config/theme/theme_data.dart';
-import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/home/home_bloc.dart';
-import 'package:store_app_clean_architecture/features/store_feature/presentation/screens/categories_screen.dart';
-import 'package:store_app_clean_architecture/features/store_feature/presentation/screens/detail_product_screen.dart';
-import 'package:store_app_clean_architecture/features/store_feature/presentation/screens/home_screen.dart';
-import 'package:store_app_clean_architecture/features/store_feature/presentation/screens/products_by_category_screen.dart';
+import 'package:store_app_clean_architecture/core/widgets/main_wrapper.dart';
 import 'package:store_app_clean_architecture/service_locator.dart';
 
 void main() async {
@@ -23,13 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: mainTheme,
-      home: Directionality(
-        textDirection: TextDirection.rtl,
-        child: BlocProvider(
-          create: (context) => serviceLocator.get<HomeBloc>(),
-          child: const HomeScreen(),
-        ),
-      ),
+      home: const MainWrapper(),
     );
   }
 }
