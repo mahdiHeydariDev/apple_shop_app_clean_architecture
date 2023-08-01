78f39ce3eac5f476c0a86da334fe31a0a04b2467
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-01 12:47:49 +0330

    add detail product to service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,14 +3,21 @@ import 'package:get_it/get_it.dart';
 import 'package:store_app_clean_architecture/core/constants/constants_api.dart';
 import 'package:store_app_clean_architecture/features/store_feature/data/data_source/banners_data_source.dart';
 import 'package:store_app_clean_architecture/features/store_feature/data/data_source/category_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/detail_product_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/gallery_product_data_source.dart';
 import 'package:store_app_clean_architecture/features/store_feature/data/data_source/products_data_source.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/repository/banners_repository_impl.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/repository/categories_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/detail_product_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/gallery_product_repository_impl.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/repository/products_repository_impl.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/banners_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/categories_use_case.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/detail_product_use_case.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/gallery_image_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/products_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/categories/categories_bloc.dart';
+import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/detail_product/detail_product_bloc.dart';
 import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/home/home_bloc.dart';
 
 var serviceLocator = GetIt.instance;
@@ -39,6 +46,16 @@ Future<void> initGeiIt() async {
       dio: serviceLocator.get(),
     ),
   );
+  serviceLocator.registerSingleton<GalleryProductDataSource>(
+    GalleryProductDataSource(
+      dio: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<DetailProductDatasource>(
+    DetailProductDatasource(
+      dio: serviceLocator.get(),
+    ),
+  );
   //repositories
   serviceLocator.registerSingleton<BannersRepositoryImpl>(
     BannersRepositoryImpl(
@@ -55,6 +72,17 @@ Future<void> initGeiIt() async {
       dataSource: serviceLocator.get(),
     ),
   );
+  serviceLocator.registerSingleton<GalleryProductRepositoryImpl>(
+    GalleryProductRepositoryImpl(
+      dataSource: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<DetailProductRepositoryImpl>(
+    DetailProductRepositoryImpl(
+      dataSource: serviceLocator.get(),
+    ),
+  );
+
   //use cases,
   serviceLocator.registerSingleton<BannersUseCase>(
     BannersUseCase(
@@ -71,6 +99,16 @@ Future<void> initGeiIt() async {
       repository: serviceLocator.get(),
     ),
   );
+  serviceLocator.registerSingleton<GalleryImagesUsecase>(
+    GalleryImagesUsecase(
+      repository: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<DetailProductUsecase>(
+    DetailProductUsecase(
+      repository: serviceLocator.get(),
+    ),
+  );
   //Bloc
   serviceLocator.registerSingleton<HomeBloc>(
     HomeBloc(
@@ -82,6 +120,13 @@ Future<void> initGeiIt() async {
   serviceLocator.registerSingleton<CategoriesBloc>(
     CategoriesBloc(
       categoriesUsecase: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<DetailProductBloc>(
+    DetailProductBloc(
+      categoryUsecase: serviceLocator.get(),
+      galleryImageUsecase: serviceLocator.get(),
+      detailProductUsecase: serviceLocator.get(),
     ),
   );
 }
