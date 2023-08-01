7dcbe09a56b38554b87da53852fb92c8653dcfa7
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-01 12:46:24 +0330

    add variants routes

---
+++ lib/core/constants/constants_routes/constants_api_routs.dart
@@ -0,0 +1,14 @@
+class ConstantsRoutesApi {
+  //get all banners
+  static const String bannersRoutes = 'collections/banner/records';
+  //get all categories
+  static const String categoryRoutes = 'collections/category/records';
+  //get all products
+  static const String productsRoutes = 'collections/products/records';
+  //get all gallery images
+  static const String galleryRoutes = 'collections/gallery/records';
+  //get all variant types
+  static const String variantTypesRoutes = 'collections/variants_type/records';
+  //get all variants
+  static const String variantsRoures = 'collections/variants/records';
+}
