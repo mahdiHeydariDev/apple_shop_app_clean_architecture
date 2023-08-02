a527552f3834d7b884c73a26539b84166856a459
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-02 16:43:46 +0330

    Add a function to get categories by id

--- lib/features/store_feature/data/data_source/category_data_source.dart
+++ lib/features/store_feature/data/data_source/category_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:store_app_clean_architecture/core/constants/constants_routes/constants_routs.dart';
+import 'package:store_app_clean_architecture/core/constants/constants_routes/constants_api_routs.dart';
 import 'package:store_app_clean_architecture/core/utils/errors/api_error.dart';
 import 'package:store_app_clean_architecture/core/utils/errors/uknown_exception.dart';
 
@@ -11,6 +11,25 @@ class CategoryDataSource {
       final Response response = await dio.get(
         ConstantsRoutesApi.categoryRoutes,
       );
+      return response;
+    } on DioException catch (exception) {
+      throw ApiEception(
+        exceptionMessage: exception.response!.statusMessage!,
+        exceptionCode: exception.response!.statusCode!,
+      );
+    } catch (_) {
+      throw UknownError();
+    }
+  }
+
+  Future<Response> getCategoryById({required String id}) async {
+    final Map<String, dynamic> queryParams = {'filter': 'id="$id"'};
+    try {
+      final Response response = await dio.get(
+        ConstantsRoutesApi.categoryRoutes,
+        queryParameters: queryParams,
+      );
+
       return response;
     } on DioException catch (exception) {
       throw ApiEception(
