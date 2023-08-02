d486aa675ef854a841ae61f0dfe02435df03e6c2
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-02 16:52:32 +0330

    Create detail product use case

---
+++ lib/features/store_feature/domain/use_cases/detail_product_use_case.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_app_clean_architecture/core/utils/errors/custom_error.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_variant_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/property_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/detail_product_repository_impl.dart';
+
+class DetailProductUsecase {
+  final DetailProductRepositoryImpl repository;
+  DetailProductUsecase({required this.repository});
+  Future<Either<CustomError, List<ProductVariantEntity>>> callGetVariants({
+    required String productId,
+  }) async {
+    return await repository.getProductVariants(productId: productId);
+  }
+
+  Future<Either<CustomError, List<PropertyEntity>>> callGetProperties({
+    required String productId,
+  }) async {
+    return await repository.getProperties(productId: productId);
+  }
+}
