ea3be5a0af2b87aeab134b57d10d9207772c3e53
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-02 16:50:33 +0330

    create variant type entity class

---
+++ lib/features/store_feature/domain/entity/variant_type_entity.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class VariantTypeEntity extends Equatable {
+  final String id;
+  final String name;
+  final String title;
+  final String type;
+  const VariantTypeEntity({
+    required this.id,
+    required this.name,
+    required this.title,
+    required this.type,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        id,
+        name,
+        title,
+        type,
+      ];
+}
