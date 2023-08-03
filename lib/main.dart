b96b7f6d475120905227c2a0f22136ec7f967525
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-03 14:00:33 +0330

    Init Hive DB and Register Adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:store_app_clean_architecture/config/theme/theme_data.dart';
 import 'package:store_app_clean_architecture/core/widgets/main_wrapper.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/order_entity.dart';
 import 'package:store_app_clean_architecture/service_locator.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(OrderEntityAdapter());
+  await Hive.openBox<OrderEntity>('orders');
   await initGeiIt();
   runApp(
     const MyApp(),
