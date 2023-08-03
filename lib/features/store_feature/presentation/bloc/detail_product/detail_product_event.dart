2a10af4c3dafa50114585d4682eb5cc780c577b6
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-03 14:08:05 +0330

    Add add to basket event

--- lib/features/store_feature/presentation/bloc/detail_product/detail_product_event.dart
+++ lib/features/store_feature/presentation/bloc/detail_product/detail_product_event.dart
@@ -1,3 +1,6 @@
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_variant_entity.dart';
+
 class DetailProductEvent {}
 
 class DetailProductSendRequestEvent extends DetailProductEvent {
@@ -6,5 +9,14 @@ class DetailProductSendRequestEvent extends DetailProductEvent {
   DetailProductSendRequestEvent({
     required this.id,
     required this.productId,
+  });
+}
+
+class DetailProductAddToBasketEvent extends DetailProductEvent {
+  final ProductEntity product;
+  final List<ProductVariantEntity> variants;
+  DetailProductAddToBasketEvent({
+    required this.product,
+    required this.variants,
   });
 }
