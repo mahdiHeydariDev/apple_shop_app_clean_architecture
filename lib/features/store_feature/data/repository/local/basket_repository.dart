d8e9d44dca4a9b9f755bd568a357c5cc55d8ea7c
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-03 14:05:04 +0330

    Create

---
+++ lib/features/store_feature/data/repository/local/basket_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_app_clean_architecture/core/utils/errors/custom_error.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/order_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_variant_entity.dart';
+
+abstract class BasketRepository {
+  Future<Either<CustomError, String>> addToBasket({
+    required ProductEntity product,
+    required List<ProductVariantEntity> variants,
+  });
+  Future<Either<CustomError, List<OrderEntity>>> getOrders();
+}
