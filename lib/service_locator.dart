86fe03c0eca035eb88cd4e603efab532c3b98bb6
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-03 14:01:51 +0330

    Add Hive and Basket

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,27 +1,37 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:store_app_clean_architecture/core/constants/constants_api.dart';
-import 'package:store_app_clean_architecture/features/store_feature/data/data_source/banners_data_source.dart';
-import 'package:store_app_clean_architecture/features/store_feature/data/data_source/category_data_source.dart';
-import 'package:store_app_clean_architecture/features/store_feature/data/data_source/detail_product_data_source.dart';
-import 'package:store_app_clean_architecture/features/store_feature/data/data_source/gallery_product_data_source.dart';
-import 'package:store_app_clean_architecture/features/store_feature/data/data_source/products_data_source.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/repository/banners_repository_impl.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/repository/categories_repository_impl.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/repository/detail_product_repository_impl.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/repository/gallery_product_repository_impl.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/repository/products_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/local/basket_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/remote/banners_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/remote/category_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/remote/detail_product_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/remote/gallery_product_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/data/data_source/remote/products_data_source.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/order_entity.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/local/basket_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/remote/banners_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/remote/categories_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/remote/detail_product_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/remote/gallery_product_repository_impl.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/repository/remote/products_repository_impl.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/banners_use_case.dart';
+import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/basket_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/categories_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/detail_product_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/gallery_image_use_case.dart';
 import 'package:store_app_clean_architecture/features/store_feature/domain/use_cases/products_use_case.dart';
+import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/basket/basket_bloc.dart';
 import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/categories/categories_bloc.dart';
 import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/detail_product/detail_product_bloc.dart';
 import 'package:store_app_clean_architecture/features/store_feature/presentation/bloc/home/home_bloc.dart';
 
 var serviceLocator = GetIt.instance;
 Future<void> initGeiIt() async {
+  //Hive
+  serviceLocator.registerSingleton<Box<OrderEntity>>(
+    Hive.box<OrderEntity>('orders'),
+  );
   //dio
   serviceLocator.registerSingleton<Dio>(
     Dio(
@@ -56,6 +66,11 @@ Future<void> initGeiIt() async {
       dio: serviceLocator.get(),
     ),
   );
+  serviceLocator.registerSingleton<BasketDataSource>(
+    BasketDataSource(
+      ordersBox: serviceLocator.get(),
+    ),
+  );
   //repositories
   serviceLocator.registerSingleton<BannersRepositoryImpl>(
     BannersRepositoryImpl(
@@ -79,6 +94,11 @@ Future<void> initGeiIt() async {
   );
   serviceLocator.registerSingleton<DetailProductRepositoryImpl>(
     DetailProductRepositoryImpl(
+      dataSource: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<BasketRepositoryImpl>(
+    BasketRepositoryImpl(
       dataSource: serviceLocator.get(),
     ),
   );
@@ -109,6 +129,11 @@ Future<void> initGeiIt() async {
       repository: serviceLocator.get(),
     ),
   );
+  serviceLocator.registerSingleton<BasketUsecase>(
+    BasketUsecase(
+      repository: serviceLocator.get(),
+    ),
+  );
   //Bloc
   serviceLocator.registerSingleton<HomeBloc>(
     HomeBloc(
@@ -127,6 +152,12 @@ Future<void> initGeiIt() async {
       categoryUsecase: serviceLocator.get(),
       galleryImageUsecase: serviceLocator.get(),
       detailProductUsecase: serviceLocator.get(),
+      basketUsecase: serviceLocator.get(),
+    ),
+  );
+  serviceLocator.registerSingleton<BasketBloc>(
+    BasketBloc(
+      basketUsecase: serviceLocator.get(),
     ),
   );
 }
