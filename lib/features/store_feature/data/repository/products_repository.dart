b778161a371603ceab4dd05ce576450d3372f48b
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-03 14:09:56 +0330

    Delete

--- lib/features/store_feature/data/repository/products_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:store_app_clean_architecture/core/utils/errors/custom_error.dart';
-import 'package:store_app_clean_architecture/features/store_feature/domain/entity/product_entity.dart';
-
-abstract class ProductsRepository {
-  Future<Either<CustomError, List<ProductEntity>>> getFilteredProducts({
-    required String filter,
-  });
-}
