edd99ef6caabcbbf1a4882b8a742eb1c2df39e78
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-04 09:23:44 +0330

    Add Increas Order Count Function

--- lib/features/store_feature/presentation/bloc/basket/basket_event.dart
+++ lib/features/store_feature/presentation/bloc/basket/basket_event.dart
@@ -1,3 +1,10 @@
+import 'package:store_app_clean_architecture/features/store_feature/domain/entity/order_entity.dart';
+
 class BasketEvent {}
 
 class BasketSendRequestEvent extends BasketEvent {}
+
+class BasketIncreseOrderCount extends BasketEvent {
+  final OrderEntity order;
+  BasketIncreseOrderCount({required this.order});
+}
