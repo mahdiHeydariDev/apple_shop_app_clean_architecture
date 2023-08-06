4abe8c5048f67fac02a7e3063c9957990eb5d2b5
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-06 17:23:03 +0330

    Add Payment Request Event

--- lib/features/store_feature/presentation/bloc/basket/basket_event.dart
+++ lib/features/store_feature/presentation/bloc/basket/basket_event.dart
@@ -4,7 +4,25 @@ class BasketEvent {}
 
 class BasketSendRequestEvent extends BasketEvent {}
 
+//ZarinPal Payment
+class BasketPaymentRequestEvent extends BasketEvent {
+  final num amount;
+  BasketPaymentRequestEvent({
+    required this.amount,
+  });
+}
+
 class BasketIncreseOrderCount extends BasketEvent {
   final OrderEntity order;
   BasketIncreseOrderCount({required this.order});
+}
+
+class BasketDecreseOrderCount extends BasketEvent {
+  final OrderEntity order;
+  BasketDecreseOrderCount({required this.order});
+}
+
+class BasketDeleteOrder extends BasketEvent {
+  final OrderEntity order;
+  BasketDeleteOrder({required this.order});
 }
