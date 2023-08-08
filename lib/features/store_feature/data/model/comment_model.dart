ee4917b5ea6fc73baf41deed2b68a14e840c9e18
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-08 19:50:47 +0330

    Add new fields

--- lib/features/store_feature/data/model/comment_model.dart
+++ lib/features/store_feature/data/model/comment_model.dart
@@ -6,11 +6,15 @@ class CommentModel extends CommentEntity {
     required String productId,
     required String text,
     required String userId,
+    required String userAvatarUrl,
+    required String userName,
   }) : super(
           id: id,
           productId: productId,
           text: text,
           userId: userId,
+          userAvatarUrl: userAvatarUrl,
+          userName: userName,
         );
 
   factory CommentModel.fromMapJson(Map<String, dynamic> mapJson) {
@@ -19,6 +23,10 @@ class CommentModel extends CommentEntity {
       productId: mapJson['product_id'],
       text: mapJson['text'],
       userId: mapJson['user_id'],
+      userAvatarUrl: mapJson['expand']['user_id']['avatar'] == ''
+          ? 'https://www.pngkit.com/png/detail/202-2024644_profile-icon-profile-color-icon-png.png'
+          : 'http://startflutter.ir/api/files/${mapJson['expand']['user_id']['collectionName']}/${mapJson['expand']['user_id']['id']}/${mapJson['expand']['user_id']['avatar']}',
+      userName: mapJson['expand']['user_id']['name'],
     );
   }
 }
