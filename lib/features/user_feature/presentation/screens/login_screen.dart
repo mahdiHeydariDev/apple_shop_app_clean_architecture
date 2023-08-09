171f24d2db2a588b36b8295267e0fe80d78cfb56
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-09 16:35:19 +0330

    Add Bloc

--- lib/features/user_feature/presentation/screens/login_screen.dart
+++ lib/features/user_feature/presentation/screens/login_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app_clean_architecture/core/constants/constant_colors.dart';
+import 'package:store_app_clean_architecture/core/constants/constants_text/form_text.dart';
+import 'package:store_app_clean_architecture/core/widgets/custom_loading.dart';
+import 'package:store_app_clean_architecture/features/user_feature/presentation/bloc/login/login_bloc.dart';
+import 'package:store_app_clean_architecture/features/user_feature/presentation/bloc/login/login_event.dart';
+import 'package:store_app_clean_architecture/features/user_feature/presentation/bloc/login/login_state.dart';
+import 'package:store_app_clean_architecture/features/user_feature/presentation/bloc/login/login_status.dart';
 import 'package:store_app_clean_architecture/features/user_feature/presentation/widgets/custom_button.dart';
 import 'package:store_app_clean_architecture/features/user_feature/presentation/widgets/custom_header.dart';
 import 'package:store_app_clean_architecture/features/user_feature/presentation/widgets/custom_text_field.dart';
@@ -17,42 +24,79 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ConstantsColors.background,
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            const CustomFormHeader(text: 'ورود به اپل لند'),
-            FormContainer(
-              fields: [
-                CustomTextField(
-                  hintText: 'نام کاربری',
-                  icon: Icons.person_outline,
-                  controller: userNameController,
-                ),
-                CustomTextField(
-                  hintText: 'رمز عبور',
-                  icon: Icons.lock_outline,
-                  controller: passwordController,
-                  isSecret: true,
-                ),
-                CustomButton(
-                  text: 'ورود',
-                  whenClick: () {},
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  'با ثبت نام در اپل لند ، قوانین و شرایط را می پذیرید.',
-                  style: TextStyle(
-                    color: ConstantsColors.grey,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: ConstantsColors.background,
+        body: SafeArea(child: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state) {
+            if (state.status is LoginLoadingStatus) {
+              return const CustomLoading();
+            }
+            if (state.status is LoginInitStatus) {
+              return CustomScrollView(
+                slivers: <Widget>[
+                  const CustomFormHeader(text: 'ورود به اپل لند'),
+                  FormContainer(
+                    fields: [
+                      CustomTextField(
+                        hintText: 'نام کاربری',
+                        icon: Icons.person_outline,
+                        controller: userNameController,
+                      ),
+                      CustomTextField(
+                        hintText: 'رمز عبور',
+                        icon: Icons.lock_outline,
+                        controller: passwordController,
+                        isSecret: true,
+                      ),
+                      (state.status as LoginInitStatus).error == 400
+                          ? const Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                              child: Text(
+                                FormText.incorrectUsername,
+                                style: TextStyle(
+                                  color: ConstantsColors.red,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            )
+                          : Container(),
+                      CustomButton(
+                        text: 'ورود',
+                        whenClick: () {
+                          BlocProvider.of<LoginBloc>(context).add(
+                            LoginSendRequestEvent(
+                              identity: userNameController.text,
+                              password: passwordController.text,
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        FormText.acceptPrivecy,
+                        style: TextStyle(
+                          color: ConstantsColors.grey,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ],
-        ),
+                ],
+              );
+            }
+            if (state.status is LoginSuccessResponseStatus) {
+              WidgetsBinding.instance.addPostFrameCallback(
+                (timeStamp) {
+                  Navigator.pop(context);
+                },
+              );
+            }
+            return Container();
+          },
+        )),
       ),
     );
   }
