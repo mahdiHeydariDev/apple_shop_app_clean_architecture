069c351fc196ca4557ef21cfc71417ac92797d01
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-14 12:36:27 +0330

    Add take user token

--- lib/features/user_feature/presentation/bloc/login/login_bloc.dart
+++ lib/features/user_feature/presentation/bloc/login/login_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:store_app_clean_architecture/core/utils/params/login_user_params.dart';
 import 'package:store_app_clean_architecture/features/user_feature/domain/use_case/login_user_use_case.dart';
 import 'package:store_app_clean_architecture/features/user_feature/presentation/bloc/login/login_event.dart';
@@ -28,6 +29,11 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         final Either<int, String> response =
             await _useCase.call(sentParams: sentParams);
         if (response.isRight()) {
+          String? userToken;
+          response.fold((l) => null, (token) => userToken = token);
+          final SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('token', userToken!);
+
           emit(
             state.setStatus(
               newStatus: LoginSuccessResponseStatus(),
