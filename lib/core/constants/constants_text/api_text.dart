4acdc53a5d1a7130853063d3d45ec3bedacd5b79
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-14 18:41:31 +0330

    Add server error texts

--- lib/core/constants/constants_text/api_text.dart
+++ lib/core/constants/constants_text/api_text.dart
@@ -4,4 +4,7 @@ class ApiText {
   static const String networkDescription =
       'اتصال اینترنتی خود را بررسی نمایید!';
   static const String uknownDescription = 'لطفاً دوباره امتحان نمایید';
+  static const String serverErrorHeader = 'خطا در برقراری ارتباط!';
+  static const String serverErrorDescription =
+      'در ارتباط با سرور خطایی رخ داده است. می تواند به دلیل بروزرسانی سرور یا خرابی آن باشد. لطفاً چند دقیقه بعد دوباره امتحان نمایید';
 }
