b86c7a57c05d0ed626cde616851bac20e1d1bdff
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-08-17 15:51:39 +0330

    Change the color

--- lib/core/constants/constant_colors.dart
+++ lib/core/constants/constant_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ConstantsColors {
   static const Color background = Color(0xffEEEEEE);
-  static const Color blue = Colors.lightBlue;
+  static const Color blue = Color(0xff2F81F7);
   static const Color grey = Color(0xff858585);
   static const Color green = Color(0xff1DB68B);
   static const Color red = Color(0xffD02026);
